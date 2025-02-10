dcb60aec30520cf3ce39d2ee9ddcf1e4f3bd073c
Larimar4you <Zajanezlaja@gmail.com>
2025-02-10 14:45:15 +0100

    git push

--- src/main.js
+++ src/main.js
@@ -3,3 +3,31 @@ import moviesData from './data/movie.json';
 const BASE_URL_IMG = 'https://image.tmdb.org/t/p/w500';
 
 console.log(moviesData.results);
+
+{
+  /* <li class="task-list-item">
+<h3>Заголовок</h3>
+<p>Текст</p>
+<button class="task-list-item-btn">Add to &#10084;</button> */
+}
+
+const taskList = document.querySelector('#task-list');
+console.log(taskList);
+
+function createMarkup(array) {
+  return array
+    .map(movie => {
+      return `  <li data-id='${movie.id}' class="task-list-item">
+      <img src="${BASE_URL_IMG}${movie.poster_path}" alt="" class="movieImage">
+      <h3>${movie.title}</h3>
+      <p>${movie.popularity}</p>
+      <button class="task-list-item-btn">Add to &#10084;</button>
+  </li>`;
+    })
+    .join('');
+}
+
+const marupMovies = createMarkup(moviesData.results);
+console.log(marupMovies);
+
+taskList.insertAdjacentHTML('beforeend', marupMovies);
